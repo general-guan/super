e1435e277c1fc673e52f329f9321393b0cd68844
guanjunjiang <934750771@qq.com>
2024-06-15 16:31:54 +0800

    链接

--- electron/ipcMain.ts
+++ electron/ipcMain.ts
@@ -1,8 +1,11 @@
-import { ipcMain } from "electron";
+import { ipcMain, shell } from "electron";
 import { createBrowserWindowFn } from "./utils";
 
 export const ipcMainInit = () => {
   ipcMain.on("createBrowserWindow", (_, config) => {
     createBrowserWindowFn(config);
+  });
+  ipcMain.on("openUrl", (_, { url }) => {
+    shell.openExternal(url);
   });
 };
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,7 @@
 <template>
   <main class="text-54px color-#cfae11">init</main>
   <button @click="translateBtn">翻译</button>
+  <button @click="linkBtn">链接</button>
 </template>
 
 <script setup lang="ts">
@@ -11,5 +12,9 @@ const translateBtn = () => {
     hash: "/translate",
     webviewTag: true,
   });
+};
+
+const linkBtn = () => {
+  ipcRenderer.send("openUrl", { url: "https://www.baidu.com/" });
 };
 </script>
